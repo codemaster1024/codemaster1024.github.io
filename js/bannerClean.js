2728a0a360ec959c0cd2809f2cfa7e3b9c2c0491
codemaser1024 <1787806000@qq.com>
2020-11-29 01:17:22 +0800

    Site updated: 2020-11-29 01:17:18

---
+++ js/bannerClean.js
@@ -0,0 +1,7 @@
+$(function(){
+    var full_page = document.getElementsByClassName("full_page");
+    if (full_page.length != 0) {
+      full_page[0].style.background = "transparent";
+    }
+
+});
